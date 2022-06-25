4e7441fdfaa355e48a1d76c93c50612b7157bdfd
Tenebroso <42720127+tenebrosoo@users.noreply.github.com>
2022-06-25 09:45:05 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+(() => {
+    const encode = (string) => {
+        return Buffer.from(string).toString('base64');
+    }
+
+    const decode = (string) => {
+        return Buffer.from(string, 'base64').toString('ascii');
+    }
+
+    console.log(encode('hello'), decode('aGVsbG8='));
+})()
